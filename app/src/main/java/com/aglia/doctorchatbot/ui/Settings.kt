5941f5b6296a415feff4176ffa7ce2cd1d62c8c9
Abaraburu <66360491+Abaraburu@users.noreply.github.com>
2024-05-27 18:46:12 +0200

    Inizio della creazione della pagina delle impostazioni. Aggiunta del bottone indietro.

--- app/src/main/java/com/aglia/doctorchatbot/ui/Settings.kt
+++ app/src/main/java/com/aglia/doctorchatbot/ui/Settings.kt
@@ -2,11 +2,22 @@ package com.aglia.doctorchatbot.ui
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.aglia.doctorchatbot.R
+import com.aglia.doctorchatbot.databinding.SettingsBinding
 
 class SettingsActivity : AppCompatActivity() {
+
+    private lateinit var binding: SettingsBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.settings)
+        binding = SettingsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        clickEvents()
+    }
+
+    private fun clickEvents() {
+        binding.backButton.setOnClickListener {
+            onBackPressed()
+        }
     }
 }
