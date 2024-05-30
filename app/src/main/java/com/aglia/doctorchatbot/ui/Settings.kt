8b4b97472b34a96fdc4e37f9f14903367aaed328
Abaraburu <66360491+Abaraburu@users.noreply.github.com>
2024-05-30 18:33:15 +0200

    Aggiunta bottone logout nelle impostazioni. Modifiche miniori alla grafica.

--- app/src/main/java/com/aglia/doctorchatbot/ui/Settings.kt
+++ app/src/main/java/com/aglia/doctorchatbot/ui/Settings.kt
@@ -1,16 +1,22 @@
 package com.aglia.doctorchatbot.ui
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.aglia.doctorchatbot.databinding.SettingsBinding
+import com.google.firebase.auth.FirebaseAuth
 
 class SettingsActivity : AppCompatActivity() {
 
     private lateinit var binding: SettingsBinding
+    private lateinit var firebaseAuth: FirebaseAuth
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = SettingsBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        firebaseAuth = FirebaseAuth.getInstance()
 
         clickEvents()
     }
@@ -18,6 +24,14 @@ class SettingsActivity : AppCompatActivity() {
     private fun clickEvents() {
         binding.backButton.setOnClickListener {
             onBackPressed()
+        }
+
+        binding.buttonLogout.setOnClickListener {
+            firebaseAuth.signOut()
+            val intent = Intent(this, SignInActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
+            startActivity(intent)
+            finish()
         }
     }
 }
